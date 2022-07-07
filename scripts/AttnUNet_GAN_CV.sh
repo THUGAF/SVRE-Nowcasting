abc306573e6ce987e2ea83aaea9445538d1a3941
THUGAF <gaf6565@163.com>
2022-07-07 12:21:03 +0800

    Add script

---
+++ scripts/AttnUNet_GAN_CV.sh
@@ -0,0 +1,20 @@
+CUDA_VISIBLE_DEVICES=1 \
+nohup python -u train.py \
+    --data-path /data/gaf/SBandCRUnzip \
+    --output-path results/AttnUNet_GAN_CV \
+    --model AttnUNet \
+    --add-gan \
+    --train \
+    --test \
+    --predict \
+    --ensemble-members 4 \
+    --sample-index 16840 \
+    --max-iterations 100000 \
+    --early-stopping \
+    --gan-reg 0.2 \
+    --var-reg 0.1 \
+    --batch-size 16 \
+    --num-threads 8 \
+    --num-workers 8 \
+    --display-interval 20 \
+    > AttnUNet_GAN_CV.log 2>&1 &
