3f2a17039429465dd2cf038025c44344aff01442
THUGAF <gaf6565@163.com>
2022-07-26 16:48:57 +0800

    Add data assimilation

--- utils/trainer.py
+++ utils/trainer.py
@@ -15,6 +15,8 @@ import utils.evaluation as evaluation
 import utils.scaler as scaler
 import model.losses as losses
 
+from utils.postprocessing import KF, EnKF
+
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
@@ -673,6 +675,106 @@ class GANTrainer:
         torch.save(states, os.path.join(self.args.output_path, filename))
 
     def load_checkpoint(self, filename='checkpoint.pt'):
-        states = torch.load(os.path.join(
-            self.args.output_path, filename), map_location=self.args.device)
+        states = torch.load(os.path.join(self.args.output_path, filename), map_location=self.args.device)
         return states
+
+
+class Assimilation:
+    def __init__(self, args):
+        self.args = args
+
+    def load_checkpoint(self, filename='checkpoint.pt'):
+        states = torch.load(os.path.join(self.args.output_path, filename))
+        return states
+
+    def evaluate(self, pred, truth, metrics):
+        for threshold in self.args.thresholds:
+            pod, far, csi, hss = evaluation.evaluate_forecast(pred, truth, threshold)
+            metrics['POD-%ddBZ' % threshold] = pod
+            metrics['FAR-%ddBZ' % threshold] = far
+            metrics['CSI-%ddBZ' % threshold] = csi
+            metrics['HSS-%ddBZ' % threshold] = hss
+
+        metrics['CC'] = evaluation.evaluate_cc(pred, truth)
+        metrics['ME'] = evaluation.evaluate_me(pred, truth)
+        metrics['MAE'] = evaluation.evaluate_mae(pred, truth)
+        metrics['RMSE'] = evaluation.evaluate_rmse(pred, truth)
+        return metrics
+    
+    def da_forward(self, x):
+        x = x[-1].flatten().expand()
+        y = self.model(x)
+        y = y[0].flatten()
+        return y[0]
+
+    def predict(self, model, sample_loader):
+        metrics = {}
+        metrics['Time'] = np.linspace(6, 60, 10)
+        self.model = model
+        self.model.load_state_dict(self.load_checkpoint('bestmodel.pt')['model'])
+        self.model.eval()
+        print('\n[Predict]')
+        with torch.no_grad():
+            # direct forecast with no observation
+            for tensor, timestamp in sample_loader:
+                tensor = tensor.transpose(1, 0)
+                timestamp = timestamp.transpose(1, 0)
+                input_ = tensor[:self.args.input_steps]
+                truth = tensor[self.args.input_steps:]
+                input_ = scaler.minmax_norm(input_, self.args.vmax, self.args.vmin)
+                truth = scaler.minmax_norm(truth, self.args.vmax, self.args.vmin)
+                
+                pred = self.model(input_)
+                input_rev = scaler.reverse_minmax_norm(input_, self.args.vmax, self.args.vmin)
+                pred_rev = scaler.reverse_minmax_norm(pred, self.args.vmax, self.args.vmin)
+                truth_rev = scaler.reverse_minmax_norm(truth, self.args.vmax, self.args.vmin)
+
+            metrics = self.evaluate(pred_rev, truth_rev, metrics)
+            df = pd.DataFrame(data=metrics)
+            df.to_csv(os.path.join(self.args.output_path, 'nonobs_metrics.csv'), float_format='%.8f', index=False)
+            visualizer.plot_map(input_rev, pred_rev, truth_rev, timestamp, self.args.output_path, 'nonobs')
+
+            # forecast with pure observation
+            pred_obs = []
+            for tensor, timestamp in sample_loader:
+                tensor = tensor.transpose(1, 0)
+                timestamp = timestamp.transpose(1, 0)
+                truth = tensor[self.args.input_steps:]
+                for s in range(self.args.forecast_steps):
+                    input_ = tensor[s: s + self.args.input_steps]
+                    input_ = scaler.minmax_norm(input_, self.args.vmax, self.args.vmin)
+
+                    pred = self.model(input_)
+                    pred_rev = scaler.reverse_minmax_norm(pred, self.args.vmax, self.args.vmin)
+                    pred_obs.append(pred_rev[0])
+            
+            input_ = tensor[:self.args.input_steps]
+            pred_obs = torch.stack(pred_obs)
+            metrics = self.evaluate(pred_obs, truth, metrics)
+            df = pd.DataFrame(data=metrics)
+            df.to_csv(os.path.join(self.args.output_path, 'obs_metrics.csv'), float_format='%.8f', index=False)
+            visualizer.plot_map(input_, pred_rev, truth, timestamp, self.args.output_path, 'obs')
+
+            # forecast with data assimilation
+            # width = self.args.lon_range[1] - self.args.lon_range[0]
+            # height = self.args.lat_range[1] - self.args.lat_range[0]
+            # pred_da = []
+            # for tensor, timestamp in sample_loader:
+            #     tensor = tensor.transpose(1, 0)
+            #     timestamp = timestamp.transpose(1, 0)
+            #     input_ = tensor[:self.args.input_steps]
+            #     input_ = scaler.minmax_norm(input_, self.args.vmax, self.args.vmin)
+            #     ys = torch.stack([truth[s].flatten() for s in range(self.args.forecast_steps)])
+                
+            #     da_model = EnKF(width * height * self.args.forecast_steps, 5, self.da_forward, x0=torch.flatten(input_))
+            #     pred, _ = da_model.forward(self.args.forecast_steps, ys)
+            #     pred = torch.mean(pred, dim=1).view(self.args.forecast_steps, self.args.forecast_steps, -1)
+            #     pred_rev = scaler.reverse_minmax_norm(pred, self.args.vmax, self.args.vmin)
+            #     for s in range(self.args.forecast_steps):
+            #         pred_da.append(pred_rev[s, s].view(1, 1, width, height))
+            
+            # pred_da = torch.stack(pred_da)
+            # metrics = self.evaluate(pred_da, truth_rev, metrics)
+            # df = pd.DataFrame(data=metrics)
+            # df.to_csv(os.path.join(self.args.output_path, 'enkf_metrics.csv'), float_format='%.8f', index=False)
+            # visualizer.plot_map(input_rev, pred_rev, truth_rev, timestamp, self.args.output_path, 'enkf')
