b11063146de961b9c80363a39d42a0bd74845c81
THUGAF <gaf6565@163.com>
2025-01-02 22:38:56 +0800

    Update loss

--- utils/losses.py
+++ utils/losses.py
@@ -6,23 +6,25 @@ import utils.transform as transform
 
 
 def biased_mse_loss(pred: torch.Tensor, truth: torch.Tensor, vmax: float, vmin: float) -> torch.Tensor:
-    points = transform.minmax_norm(torch.tensor([10.0, 20.0, 30.0, 40.0]), vmax, vmin)
+    points = transform.minmax_norm(torch.tensor([10.0, 20.0, 30.0, 40.0]).to(pred.device), vmax, vmin)
     weight = (truth < points[0]) * 1 \
         + (torch.logical_and(truth >= points[0], truth < points[1])) * 2 \
         + (torch.logical_and(truth >= points[1], truth < points[2])) * 5 \
         + (torch.logical_and(truth >= points[2], truth < points[3])) * 10 \
         + (truth >= points[3]) * 30
-    return torch.mean(weight * (pred - truth) ** 2)
+    loss = torch.mean(weight * (pred - truth) ** 2)
+    return loss
 
 
 def biased_mae_loss(pred: torch.Tensor, truth: torch.Tensor, vmax: float, vmin: float) -> torch.Tensor:
-    points = transform.minmax_norm(torch.tensor([10.0, 20.0, 30.0, 40.0]), vmax, vmin)
+    points = transform.minmax_norm(torch.tensor([10.0, 20.0, 30.0, 40.0]).to(pred.device), vmax, vmin)
     weight = (truth < points[0]) * 1 \
         + (torch.logical_and(truth >= points[0], truth < points[1])) * 2 \
         + (torch.logical_and(truth >= points[1], truth < points[2])) * 5 \
         + (torch.logical_and(truth >= points[2], truth < points[3])) * 10 \
         + (truth >= points[3]) * 30
-    return torch.mean(weight * torch.abs(pred - truth))
+    loss = torch.mean(weight * torch.abs(pred - truth))
+    return loss
 
 
 def cv_loss(pred: torch.Tensor, truth: torch.Tensor, eps: float = 1e-8) -> torch.Tensor:
